8d69d5a5b71bb264a7e26b8fd828c4e857ee07d7
Patrick Oliver <poliver24@hotmail.com>
2020-03-23 16:59:22 +0000

    added list of spaces; create space link; sign-up form; book space link in user spaces list

--- app.rb
+++ app.rb
@@ -4,37 +4,43 @@ class MakersBnB < Sinatra::Base
   enable :sessions, :method_override
 
   get '/' do
-    erb: index
+    erb :index
   end
 
   get '/sign_up' do
-    erb: sign_up
+    erb :'users/sign_up'
   end
 
   post '/sign_up' do
-    # Saves into users DB
+    # Saves into users DB (params - Name, email, username password)
+    # Update parameter names when decided 
+    User.new(name: params[:name], email: params[:email], password: params[:password])
     redirect '/user'
   end
 
   get '/user' do
-    erb: user
+    erb :user
   end
 
   get '/create_space' do
-    erb: create_space
+    erb :'spaces/create_space'
   end
 
   post '/create_space' do
     # Saves into spaces DB
+    # Update parameter names when decided 
+    Space.new(space_name: params[:space_name], description: params[:description], price: params[:price], user_id: params[:user_id])
     redirect '/user'
   end
 
-  get '/book_space' do
-    erb: book
+  get '/book_space/:space_id' do
+    @space_id = params[:space_id]
+    erb :'spaces/book'
   end
 
-  post '/book_space' do
+  post '/book_space/:space_id' do
     # change availability in spaces DB
+    Space.book(space: params[:space_id])
     redirect '/user'
   end
 
