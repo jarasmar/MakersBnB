28236d4f59078325efbb04f2eb2cd7a363c47411
Jara Santamaria <jarasmar@gmail.com>
2020-03-23 15:13:57 +0000

    setting up project files

--- app.rb
+++ app.rb
@@ -1,16 +1,11 @@
 require 'sinatra'
 
-
 class MakersBnB < Sinatra::Base
   enable :sessions, :method_override
 
   get '/' do
     "Hello World!"
   end
-
-
-
-
 
   run! if app_file == $PROGRAM_NAME
 end
