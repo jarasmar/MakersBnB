873d30f7684cbf983c5a9190c85ccf004de978d2
Ben Galley <bengalley21@gmail.com>
2020-03-24 11:47:35 +0000

    added availability column in db/migrations database setup file

--- db/migrations/02_create_spaces_table.sql
+++ db/migrations/02_create_spaces_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE spaces(space_id SERIAL PRIMARY KEY, space_name VARCHAR(60) NOT NULL, description VARCHAR(500), price DECIMAL(6, 2) NOT NULL, user_id INTEGER REFERENCES users(user_id));
+CREATE TABLE spaces(space_id SERIAL PRIMARY KEY, space_name VARCHAR(60) NOT NULL, description VARCHAR(500), price DECIMAL(6, 2) NOT NULL, available? BOOLEAN DEFAULT TRUE, user_id INTEGER REFERENCES users(user_id));
